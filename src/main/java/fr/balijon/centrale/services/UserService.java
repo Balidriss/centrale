0387b9e4e5849112ceef97f333b0036c14524050
IB <i.balijon@gmail.com>
2024-09-25 12:07:13 +0200

    ongoing service

--- src/main/java/fr/balijon/centrale/services/UserService.java
+++ src/main/java/fr/balijon/centrale/services/UserService.java
@@ -1,7 +1,8 @@
 package fr.balijon.centrale.services;
 
-import fr.balijon.centrale.entities.User;
-import fr.balijon.centrale.entities.dto.UserDTO;
+
+import fr.balijon.centrale.entity.User;
+import fr.balijon.centrale.entity.dto.UserDTO;
 import fr.balijon.centrale.repository.UserRepository;
 import fr.balijon.centrale.services.interfaces.ServiceListInterface;
 import lombok.AllArgsConstructor;
@@ -23,9 +24,9 @@ public class UserService implements ServiceListInterface<User, String, UserDTO, 
 
     @Override
     public User create(UserDTO o) {
-        User student = new User();
+        User user = new User();
         //TODO
-        return userRepository.saveAndFlush(student);
+        return userRepository.saveAndFlush(user);
     }
 
     @Override
