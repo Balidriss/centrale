2bed2ed1e0a7027df565ac8fd64e1c36f4a1d9ff
IB <i.balijon@gmail.com>
2024-09-25 10:16:01 +0200

    Base UserService

--- src/main/java/fr/balijon/centrale/services/UserService.java
+++ src/main/java/fr/balijon/centrale/services/UserService.java
@@ -1,11 +1,14 @@
 package fr.balijon.centrale.services;
 
 import fr.balijon.centrale.entities.User;
+import fr.balijon.centrale.entities.dto.UserDTO;
 import fr.balijon.centrale.repository.UserRepository;
 import fr.balijon.centrale.services.interfaces.ServiceListInterface;
 import lombok.AllArgsConstructor;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -13,4 +16,33 @@ public class UserService implements ServiceListInterface<User, String, UserDTO, 
 
     public UserRepository userRepository;
 
+    @Override
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO o) {
+        User student = new User()
+        //TODO
+        return userRepository.saveAndFlush(student);
+    }
+
+    @Override
+    public User update(UserDTO o, String id) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public void delete(String id) {
+        //TODO
+
+    }
+
+    @Override
+    public User findOneById(String id) {
+        //TODO
+        return null;
+    }
 }
