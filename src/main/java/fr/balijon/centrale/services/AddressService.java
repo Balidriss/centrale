331a2cd57fc7bc6eaf191be4b59c4dfd702c23b3
IB <i.balijon@gmail.com>
2024-09-25 13:05:25 +0200

    address & listing  services

---
+++ src/main/java/fr/balijon/centrale/services/AddressService.java
@@ -0,0 +1,51 @@
+package fr.balijon.centrale.services;
+
+
+import fr.balijon.centrale.entity.Address;
+import fr.balijon.centrale.entity.User;
+import fr.balijon.centrale.entity.dto.UserDTO;
+import fr.balijon.centrale.repository.AddressRepository;
+import fr.balijon.centrale.repository.UserRepository;
+import fr.balijon.centrale.services.interfaces.ServiceListInterface;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class AddressService implements ServiceListInterface<Address, Long, Address, Address> {
+
+    public AddressRepository addressRepository;
+
+    @Override
+    public List<Address> list() {
+        return addressRepository.findAll();
+    }
+
+    @Override
+    public Address create(Address o) {
+        Address address = new Address();
+    //TODO
+        return addressRepository.saveAndFlush(address);
+    }
+
+    @Override
+    public Address update(Address o, Long id) {
+        Address address = addressRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+    //TODO
+        return addressRepository.saveAndFlush(address);
+    }
+
+    @Override
+    public void delete(Long id) {
+        addressRepository.delete(findOneById(id));
+    }
+
+
+    @Override
+    public Address findOneById(Long id) {
+        return  addressRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+}
--- src/main/java/fr/balijon/centrale/services/ListingService.java
+++ src/main/java/fr/balijon/centrale/services/ListingService.java
@@ -1,14 +1,17 @@
 package fr.balijon.centrale.services;
 
+
+
+
 import fr.balijon.centrale.entity.Listing;
+import fr.balijon.centrale.repository.ListingRepository;
 import fr.balijon.centrale.services.interfaces.ServiceListInterface;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
-
-//TODO
 
 @Service
 @AllArgsConstructor
@@ -24,24 +27,35 @@ public class ListingService implements ServiceListInterface<Listing, Long, Listi
     @Override
     public Listing create(Listing o) {
         Listing listing = new Listing();
-
+        listing.setFavorites(o.getFavorites());
+        listing.setImages(o.getImages());
+        listing.setModel(o.getModel());
+        listing.setCreatedAt(LocalDateTime.now());
+        listing.setDescription(o.getDescription());
+        listing.setFuel(o.getFuel());
+        listing.setPrice(o.getPrice());
+        listing.setProducedAt(o.getProducedAt());
+        listing.setTitle(o.getTitle());
+        listing.setOwner(o.getOwner());
+        listing.setAddress(o.getAddress());
+        listing.setMileage(o.getMileage());
         return listingRepository.saveAndFlush(listing);
     }
 
     @Override
     public Listing update(Listing o, Long id) {
-
+        Listing listing = new Listing();
         return listingRepository.saveAndFlush(listing);
     }
 
     @Override
-    public void delete(String id) {
+    public void delete(Long id) {
         listingRepository.delete(findOneById(id));
     }
 
 
     @Override
-    public Listing findOneById(String id) {
+    public Listing findOneById(Long id) {
         return  listingRepository.findById(id).orElseThrow(EntityNotFoundException::new);
     }
 }
