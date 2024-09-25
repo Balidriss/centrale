f11d78bbd0fb1a691cca0304c3923a6034703142
IB <i.balijon@gmail.com>
2024-09-25 12:00:51 +0200

    renamed entities to entity

--- src/main/java/fr/balijon/centrale/repository/UserRepository.java
+++ src/main/java/fr/balijon/centrale/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package fr.balijon.centrale.repository;
 
-import fr.balijon.centrale.entities.User;
+import fr.balijon.centrale.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
