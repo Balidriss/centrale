0a4da7b148305cfdbd5c6a0b90b0c2c8b54a977c
IB <i.balijon@gmail.com>
2024-09-25 09:58:18 +0200

    user repo (add @)

--- src/main/java/fr/balijon/centrale/repository/UserRepository.java
+++ src/main/java/fr/balijon/centrale/repository/UserRepository.java
@@ -2,6 +2,8 @@ package fr.balijon.centrale.repository;
 
 import fr.balijon.centrale.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
 }
