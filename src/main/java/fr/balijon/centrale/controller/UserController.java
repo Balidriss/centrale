9cca208f7d0c02e707fc2ecad2d15a95d5b382fa
IB <i.balijon@gmail.com>
2024-09-26 09:58:36 +0200

    @valid on controllers

--- src/main/java/fr/balijon/centrale/controller/UserController.java
+++ src/main/java/fr/balijon/centrale/controller/UserController.java
@@ -6,10 +6,12 @@ import fr.balijon.centrale.entity.dto.UserDTO;
 import fr.balijon.centrale.entity.dto.UserUpdateDTO;
 import fr.balijon.centrale.jsonviews.JsonViews;
 import fr.balijon.centrale.services.UserService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 @RestController
 @AllArgsConstructor
@@ -19,19 +21,19 @@ public class UserController {
 
     @PostMapping
     @JsonView(JsonViews.UserShow.class)
-    public User create(@RequestBody UserDTO dto) {
+    public User create(@Valid @RequestBody UserDTO dto) {
         return userService.create(dto);
     }
 
     @PutMapping("/{uuid}")
     @JsonView(JsonViews.UserShow.class)
-    public User update(@RequestBody UserUpdateDTO dto, @PathVariable String uuid) {
+    public User update(@Valid @RequestBody UserUpdateDTO dto, @PathVariable String uuid) {
         return userService.update(dto, uuid);
     }
 
     @GetMapping("/activate/{code}")
     @JsonView(JsonViews.UserShow.class)
-    public User activate(@PathVariable String code) {
+    public User activate(@PathVariable String code) throws TimeoutException {
         return userService.activate(code);
     }
     //    @GetMapping(value = "/{slug}", name = "show")
