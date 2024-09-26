3582316a9e9b8319f2d7ad335a8b0b55cd0dba05
IB <i.balijon@gmail.com>
2024-09-26 09:57:10 +0200

    add delete mapping for favorite

--- src/main/java/fr/balijon/centrale/controller/FavoriteController.java
+++ src/main/java/fr/balijon/centrale/controller/FavoriteController.java
@@ -3,6 +3,7 @@ package fr.balijon.centrale.controller;
 import fr.balijon.centrale.entity.Favorite;
 import fr.balijon.centrale.entity.dto.FavoriteDTO;
 import fr.balijon.centrale.services.FavoriteService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,9 +17,14 @@ public class FavoriteController {
     private FavoriteService favoriteService;
 
     @PostMapping(name = "create")
-    public Favorite create(@RequestBody FavoriteDTO favorite)
+    public Favorite create(@Valid @RequestBody FavoriteDTO favorite)
     {
         return favoriteService.create(favorite);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable Long id) {
+        return favoriteService.delete(id);
     }
 
 //   @DeleteMapping(name="delete")
